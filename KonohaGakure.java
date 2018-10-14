3b111521a8493c51214279d01e1f0f78e376b05d
muhamien12 <44141758+muhamien12@users.noreply.github.com>
2018-10-14 23:57:46 +0700

    Add files via upload

---
+++ KonohaGakure.java
@@ -0,0 +1,28 @@
+public class KonohaGakure {
+  public static void main (String[]args) {
+      Shinobi [] m = new Shinobi [3];
+
+      m[0] = new Shinobi ();
+      m[0].setNoregistrasi("012606");
+      m[0].setNama("Uciha Sasuke");
+      m[0].setPeringkat("Genin");
+
+      m[1] = new Shinobi ();
+      m[1].setNoregistrasi("012607");
+      m[1].setNama("Uzumaki Naruto");
+      m[1].setPeringkat("Genin");
+
+      m[2] = new Shinobi ();
+      m[2].setNoregistrasi("012601");
+      m[2].setNama("Haruno Sakura");
+      m[2].setPeringkat("Genin");
+
+      System.out.println("Data Shinobi \n");
+      for (Shinobi x:m){
+      System.out.println("setNoregistrasi :" +x.getNoregistrasi ());
+      System.out.println("nama : "+.getNama());
+      System.out.println("peringkat:" +x.getPeringkat());
+      System.out.println();
+      }
+  }
+}
---
+++ Shinobi.java
@@ -0,0 +1,21 @@
+public class Shinobi {
+      private String noregistrasi, nama, peringkat;
+      public void setNoregistrasi (String setNoregistrasi) {
+        this.noregistrasi = no noregistrasi;
+      }
+      public String getNoregistrasi () {
+        return noregistrasi;
+      }
+      public void setNama (String nama) {
+        this.namac= nama;
+      }
+      public String getNmaa () {
+        return nama;
+      }
+      public void setPeringkat ( String peringkat) {
+        this.peringkat = peringkat;
+      }
+      public String getPeringkat () {
+        return peringkat;
+      }
+}
